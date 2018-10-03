526129fa8b3e438396dc6b6e6c42f12f77291055
Maksim <maks.yermakou@gmail.com>
2018-10-03 15:01:18 +0300

    My solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,74 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.arrValue = [initialValue];
   }
 
   add(number) {
-    // your implementation
+    this.arrValue.push('+', number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.arrValue.push('-', number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.arrValue.push('*', number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.arrValue.push('/', number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.arrValue.push('**', number);
+    return this;
+  }
+
+  valueOf(){
+    let arrPrior = ['+', '-', '*', '/'];
+    let iterPrior = 4;
+    
+    for(let i = this.arrValue.length-1; i > 0; i--){
+
+      if(this.arrValue[i] == '**'){
+        
+        this.arrValue.splice(i-1, 3, Math.pow(this.arrValue[i-1], this.arrValue[i+1]));
+        i++;
+      }
+    }
+    
+    do{
+      
+      for(let i = 0; i<this.arrValue.length; i++){
+        
+        if(this.arrValue[i] == arrPrior[iterPrior-1]){
+          
+          switch(arrPrior[iterPrior-1]){
+            case '*':
+              this.arrValue.splice(i-1, 3, this.arrValue[i-1]*this.arrValue[i+1]);
+              break;
+            case '/':
+              this.arrValue.splice(i-1, 3, this.arrValue[i-1]/this.arrValue[i+1]);
+              break;
+            case '+':
+              this.arrValue.splice(i-1, 3, this.arrValue[i-1]+this.arrValue[i+1]);
+              break;
+            case '-':
+              this.arrValue.splice(i-1, 3, this.arrValue[i-1]-this.arrValue[i+1]);
+              break;
+          }
+          i--;
+        }
+      }
+
+      iterPrior--;
+    }while(iterPrior != 0);
+
+    return this.arrValue.pop();
   }
 }
 
